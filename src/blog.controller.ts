a81ff3322b97ff781fdc1d37b563b60cffd5ea10
wjdwwidz <wjdwwidz@gmail.com>
2024-04-17 17:31:27 +0900

    Feat : Add blogController methods

--- src/blog.controller.ts
+++ src/blog.controller.ts
@@ -1,10 +1,44 @@
-import {Controller} from '@nestjs/common'
+import { Controller, Param, Body, Delete, Get, Post, Put} from '@nestjs/common';
 import { BlogService } from './blog.service'
 
 @Controller('blog')
 export class BlogController{
     blogService : BlogService;
     constructor(){
-        this.blogservice = new BlogService();
+        this.blogService = new BlogService();
     }
+
+    @Get()
+    getAllPosts(){
+        console.log('모든 게시글 가져오기');
+        return this.blogService.getAllPosts();
+    }
+
+    @Post()
+    CreatePost(@Body() postDto){ //HTTP 요청의 body 내용을 post 에 할당
+        console.log('게시글 작성');
+        this.blogService.createPost(postDto);
+        return 'success';
+    }
+
+    @Get('/:id')
+    getPost(@Param('id') id:String){
+        console.log('게시글 하나 가져오기');
+        this.blogService.getPost(id);
+    }
+
+    @Delete('/:id')
+    deletePost(@Param('id') id:String){
+        console.log('게시글 삭제');
+        this.blogService.delete(id);
+        return 'success';
+    }
+    
+    @Put('/:id')
+    updatePost(@Param('id') id, @Body() postDto){
+        console.log('게시글 업데이트', postDto);
+        return this.blogService.updatePost(id, postDto);
+
+    }
+
 }
