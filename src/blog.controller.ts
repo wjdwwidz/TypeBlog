53a117ab454dfd21634146241ab6f04192285fcd
wjdwwidz <wjdwwidz@gmail.com>
2024-04-17 16:53:22 +0900

    Chore: Add blog service member to BlogController class

--- src/blog.controller.ts
+++ src/blog.controller.ts
@@ -1,4 +1,10 @@
 import {Controller} from '@nestjs/common'
+import { BlogService } from './blog.service'
 
 @Controller('blog')
-export class BlogController{}
+export class BlogController{
+    blogService : BlogService;
+    constructor(){
+        this.blogservice = new BlogService();
+    }
+}
