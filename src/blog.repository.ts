a168f3fafa07a396d8796a99462bbc000f410dea
wjdwwidz <wjdwwidz@gmail.com>
2024-04-19 13:53:16 +0900

    feat: Added persistence layer (FILE)

--- src/blog.repository.ts
+++ src/blog.repository.ts
@@ -1,9 +1,51 @@
-import {readFile, writeFile } from 'fs/promises';
+
+import { readFile, writeFile } from 'fs/promises';
 import { PostDto } from './blog.model';
 
-//블로그 리포지토리 인터페이스 정의
 export interface BlogRepository {
-    getAllPost() : Promise<PostDto[]>;
-    createPost(postDto)
+    getAllPost(): Promise<PostDto>;
+    createPost(postDto : PostDto);
+    getPost(id: String): Promise<PostDto>;
+    deletePost(id: String);
+    updatePost(id: String, postDto : PostDto);
+}
 
+export class BlogFileRepository implements BlogRepository {
+    FILE_NAME = './src/blog.data.json'
+
+    async getAllPost(): Promise<PostDto[]> {
+        const datas = await readFile(this.FILE_NAME, 'utf8');
+        const posts = JSON.parse(datas);
+        return posts;
+        
+    }
+
+    async createPost(postDto: PostDto) {
+        const posts = await this.getAllPost();
+        const id = posts.length + 1;
+        const createPost = { id : id.toString(), ...postDto, createdDt : new Date() };
+        posts.push(createPost);
+        await writeFile(this.FILE_NAME, JSON.stringify(posts));
+        
+    }
+
+    async getPost(id: String) :Promise<PostDto> {
+        const posts = await this.getAllPost();
+        const result = posts.find((post) => post.id === id);
+        return result
+    }
+
+    async deletePost(id: String) {
+        const posts = await this.getAllPost();
+        const filteredPosts = posts.filter((post) => post.id !== id );
+        await writeFile(this.FILE_NAME, JSON.stringify(filteredPosts));
+    }
+
+    async updatePost(id: String, postDto : PostDto) {
+        const posts = await this.getAllPost();
+        const index = posts.findIndex((post) => post.id === id);
+        const updatePost = { id, ...postDto, updatedDt : new Date()};
+        posts[index] = updatePost;
+        await writeFile( this.FILE_NAME, JSON.stringify(posts));
+    }
 }
