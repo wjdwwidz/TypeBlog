460261f5ac72124f60cee1e5e28b55328611220f
wjdwwidz <wjdwwidz@gmail.com>
2024-05-11 18:20:38 +0900

    Refactor: build

--- src/blog.controller.ts
+++ src/blog.controller.ts
@@ -12,34 +12,34 @@ export class BlogController{
         return this.blogService.getAllPosts();
     }
 
-    @Post()
-    CreatePost(@Body() postDto){ //HTTP 요청의 body 내용을 post 에 할당
-        console.log('게시글 작성');
-        this.blogService.createPost(postDto);
-        return 'success';
-    }
-
-    @Get('/:id')
-    async getPost(@Param('id') id:String){
-        console.log('게시글 하나 가져오기');
-
-        const post = await this.blogService.getPost(id);
-        console.log(post);
-        return post;
-    }
-
-    @Delete('/:id')
-    deletePost(@Param('id') id:String){
-        console.log('게시글 삭제');
-        this.blogService.delete(id);
-        return 'success';
-    }
-    
-    @Put('/:id')
-    updatePost(@Param('id') id, @Body() postDto){
-        console.log(`[${id}] 게시글 업데이트`, postDto);
-        return this.blogService.updatePost(id, postDto);
-
-    }
+    // @Post()
+    // CreatePost(@Body() postDto){ //HTTP 요청의 body 내용을 post 에 할당
+    //     console.log('게시글 작성');
+    //     this.blogService.createPost(postDto);
+    //     return 'success';
+    // }
+    //
+    // @Get('/:id')
+    // async getPost(@Param('id') id:String){
+    //     console.log('게시글 하나 가져오기');
+    //
+    //     const post = await this.blogService.getPost(id);
+    //     console.log(post);
+    //     return post;
+    // }
+    //
+    // @Delete('/:id')
+    // deletePost(@Param('id') id:String){
+    //     console.log('게시글 삭제');
+    //     this.blogService.delete(id);
+    //     return 'success';
+    // }
+    //
+    // @Put('/:id')
+    // updatePost(@Param('id') id, @Body() postDto){
+    //     console.log(`[${id}] 게시글 업데이트`, postDto);
+    //     return this.blogService.updatePost(id, postDto);
+    //
+    // }
 
 }
--- src/blog.service.ts
+++ src/blog.service.ts
@@ -1,31 +1,33 @@
 import { PostDto } from './blog.model';
-import { BlogFileRepository, BlogRepository } from './blog.repository';
+// import { BlogFileRepository, BlogRepository } from './blog.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BlogService {
-    constructor(private blogRepository: BlogFileRepository){}
+    // constructor(private blogRepository: BlogFileRepository){}
 
-    posts = [];
-
-    async getAllPosts(){
-        return await this.blogRepository.getAllPost();
-    }
-    
-    createPost(postDto: PostDto){
-        this.blogRepository.createPost(postDto);
+    async getAllPosts() {
+        return await JSON.parse("{}")
     }
 
-    async getPost(id){
-    return await this.blogRepository.getPost(id);
-    }
-
-    delete(id){
-        this.blogRepository.deletePost(id);
-    }
-
-    updatePost (id, postDto : PostDto){
-        this.blogRepository.updatePost(id, postDto);
-    }
+    // async getAllPosts(){
+    //     return await this.blogRepository.getAllPost();
+    // }
+    //
+    // createPost(postDto: PostDto){
+    //     this.blogRepository.createPost(postDto);
+    // }
+    //
+    // async getPost(id){
+    // return await this.blogRepository.getPost(id);
+    // }
+    //
+    // delete(id){
+    //     this.blogRepository.deletePost(id);
+    // }
+    //
+    // updatePost (id, postDto : PostDto){
+    //     this.blogRepository.updatePost(id, postDto);
+    // }
 }
 
