bcfaf829a1120d353b3c2303908e2b296a6a4f05
wjdwwidz <wjdwwidz@gmail.com>
2024-05-13 18:22:02 +0900

    Feat : Connect MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
 import {BlogController} from './blog.controller';
 import {BlogService} from './blog.service';
-import {BlogFileRepository} from './blog.repository';
+import {BlogFileRepository, BlogMongoRepository} from './blog.repository';
+import {Blog, BlogSchema} from "./blog.schema";
 
 @Module({
-    imports: [], //외부 모듈 호출시 사용
+    imports: [
+        MongooseModule.forRoot(
+            'mongodb+srv://wjdwwidz:98EnUav1hag183S2@typeblog.2mo5hrp.mongodb.net/?retryWrites=true&w=majority&appName=TypeBlog',
+        ),
+        MongooseModule.forFeature([ { name : Blog.name, schema : BlogSchema }]),
+    ], //외부 모듈 호출시 사용
     controllers : [BlogController],
-    providers : [BlogService, BlogFileRepository],
+    providers : [BlogService, BlogFileRepository,BlogMongoRepository],
 })
 export class AppModule {}
